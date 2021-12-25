5367ea4be6084db6d9366d6c2b76691b8f6480a7
yuanweibo <2435722121@qq.com>
2021-12-25 17:36:04 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [data, setData] = useState([1, 2, 3, 4, 5])
+  const [data, setData] = useState([1, 2, 3, 4, 5]);
+
+  useEffect(() => {});
   return (
-      <div className="App">
-        {
-          data.map((item, index) => <div key={index}>{item}</div>)
-        }
-      </div>
-  )
+    <div className="App">
+      {data.map((item, index) => (
+        <div
+          key={index}
+          onClick={() => {
+            setData([9, 6, 3, 5, 78, 5, 9]);
+          }}
+        >
+          {item}
+          <div />
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
