f8170addb30475350a34600dcf8a03947a6c6d2d
yuanweibo <2435722121@qq.com>
2021-12-28 16:26:53 +0800

    配置项目

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,16 @@
-import React, { useState, useEffect } from 'react';
-
-function getList(): Promise<any> {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve([9, 8, 7, 6, 5, 3, 3, 1]);
-    }, 2000);
-  });
-}
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import routes from './router';
 
 function App() {
-  const [data, setData] = useState([1, 2, 3, 4, 5]);
-
-  useEffect(() => {
-    (async () => {
-      const data = await getList();
-      console.log(data);
-      setData(data);
-    })();
-  }, []);
-
   return (
-    <div className="App">
-      {data.map((item, index) => (
-        <div
-          key={index}
-          onClick={() => {
-            setData([9, 6, 3, 5, 78, 5, 9]);
-          }}
-        >
-          {item}
-          <div />
-        </div>
-      ))}
-    </div>
+    <Router>
+      <Routes>
+        {routes.map((route) => (
+          <Route key={route.path} path={route.path} element={<route.component />} />
+        ))}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/router/index.ts
@@ -0,0 +1,15 @@
+import Index from '@/Index';
+import About from '@/About';
+
+const routes = [
+  {
+    path: '/',
+    component: Index,
+  },
+  {
+    path: '/about',
+    component: About,
+  },
+];
+
+export default routes;
